c10261858db3ace7703b75262879fbdb014056b3
VIKASH MANDLOI <126850164+its-vikash07@users.noreply.github.com>
2024-04-19 15:44:02 +0530

    Add files via upload

---
+++ StringBufferTest.java
@@ -0,0 +1,7 @@
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Vikash");
+        sb.append(" Mandloi");
+        System.out.println(sb);
+    }
+}
---
+++ StringBuilderTest.java
@@ -0,0 +1,7 @@
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Vikash ");
+        sb.append("Mandloi");
+        System.out.println(sb);
+    }
+}
